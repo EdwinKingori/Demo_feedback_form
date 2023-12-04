72cc46459e8c2c5cfaffd6b3837a1840bbcb50a1
EDDY <eddmunyiri@gmail.com>
2023-12-04 20:22:06 +0300

    Manual Form Validation

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 
 def review(request):
-    return render(request, 'reviews/review.html')
+    if request.method == 'POST':
+        entered_username = request.POST['username']
+
+        if entered_username == "":
+            return render(request, 'reviews/review.html', {
+                'has_error': True
+            })
+        print(entered_username)
+        return HttpResponseRedirect('/thank_you')
+    return render(request, 'reviews/review.html', {
+        "has_error": False
+    })
+
+
+def thank_you(request):
+    return render(request, 'reviews/thank_you.html')
